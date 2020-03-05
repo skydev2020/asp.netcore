a43c9d72dfb879223cb5405e3a8d69e2be426750
Sky Dev <skydev2020@outlook.com>
2020-03-05 09:36:02 -0500

    startup pipeline

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -28,25 +29,45 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    //await context.Response.WriteAsync("Hello World!");
-                    await context.Response
-                    .WriteAsync(_config["MyKey"]);
-
-                });
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Response");
             });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Response");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("MW3: Request handled and response produced");
+                logger.LogInformation("MW3: Request handled and response produced");
+            });
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        //await context.Response.WriteAsync("Hello World!");
+            //        await context.Response
+            //        .WriteAsync(_config["MyKey"]);
+
+            //    });
+            //});
         }
     }
 }
